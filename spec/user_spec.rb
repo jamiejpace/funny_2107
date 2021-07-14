435e38934233eb2e8fc3856f296b43b82546c2a0
Jamie Pace <jamiejpace@gmail.com>
2021-07-14 14:24:11 -0600

    Add to user test

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -8,4 +8,30 @@ RSpec.describe User do
     expect(user_1).to be_a(User)
   end
 
+  it 'has a name' do
+    user_1 = User.new("Sal")
+
+    expect(user_1.name).to eq("Sal")
+  end
+
+  it 'has jokes' do
+    user_1 = User.new("Sal")
+
+    expect(user_1.jokes).to eq([])
+  end
+
+  it 'learns jokes' do
+    user_1 = User.new("Sal")
+    joke_1 = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    Joke.new(13, "How do you keep a lion from charging?", "Take away its credit cards.")
+    user_1.learn(joke_1)
+
+    expect(user_1.jokes.length).to eq(1)
+
+    user_1.learn(joke_2)
+
+    expect(user_1.jokes.length).to eq(2)
+    expect(user_1.jokes).to eq([joke_1, joke_2])
+  end
+
 end
