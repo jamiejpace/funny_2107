3e7d127fc2a0278164590a6e378e8bbdc5c6e180
Jamie Pace <jamiejpace@gmail.com>
2021-07-14 15:52:44 -0600

    Attempt to add repeated joke method

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -9,4 +9,20 @@ class OpenMic
   def location
     self.event_info[:location]
   end
+
+  def date
+    self.event_info[:date]
+  end
+
+  def welcome(performer)
+    @performers << performer
+  end
+
+  def repeated_jokes?
+    jokes_by_performer = performers.map do |performer|
+      performer.jokes
+    end
+    jokes_by_performer.uniq.length != jokes_by_performer.length
+  end
+
 end
